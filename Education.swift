bf567876028ab56c043bbb6a95c94c9990798da3
Juli <vovjulip@gmail.com>
2024-07-12 13:24:54 +0300

    Add education file

---
+++ Education.swift
@@ -0,0 +1,28 @@
+# Swift
+## Basics
+
+# Xcode - Playground
+
+#Swift
+```
+Collections
+Functions and closures
+Structs, Classes, Enums
+Extensions
+Protocols
+PropertyWrappers
+ARC
+Generics
+```
+
+### Coding style conventions
+
+
+### OOP Basics
+```
+Inheritance
+Incapsulations
+Polimorphism
+```
+
+
