340e503102d5da65f216b44ab2514a3a426711b7
Juli <vovjulip@gmail.com>
2024-07-19 23:10:47 +0300

    Add file loops

---
+++ ControlFlow - Loops.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var sum = 0
+
+for index in 1...5 {
+    sum += index
+    print("sum = \(sum), index = \(index)")
+    
+}
+
+print("result = \(sum)")
