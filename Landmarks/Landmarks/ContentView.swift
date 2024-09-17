6b0b284895773b953b875471ddd63d3d66466ce5
Juli <vovjulip@gmail.com>
2024-09-17 15:12:59 +0300

    Add project Landmarks

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Juli Vovnianko on 16.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+            Text("Turtle Rock")
+            .font(.title)
+            .foregroundColor(.green)
+        }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
