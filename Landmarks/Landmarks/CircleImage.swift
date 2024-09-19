2e13a05be31d9d8f8c80020d07071a8d72798da7
Juli <vovjulip@gmail.com>
2024-09-19 17:03:57 +0300

    Add new file in Landmarks

---
+++ Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,20 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Juli Vovnianko on 19.09.2024.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
--- Landmarks/Landmarks/ContentView.swift
+++ Landmarks/Landmarks/ContentView.swift
@@ -9,9 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        VStack(alignment: .leading) {
             Text("Turtle Rock")
-            .font(.title)
-            .foregroundColor(.green)
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
+            
         }
 }
 
